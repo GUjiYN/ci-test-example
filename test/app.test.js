37fe6efb218a38d6eadbe623dd841c26ea11096a
GUjiYN <2946459904@qq.com>
2025-04-02 17:25:37 +0800

    Add CI configuration

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,12 @@
-const chai = require('chai');
+import chai from 'chai';
+
 const expect = chai.expect;
 const add = require('../app');
-
 
 describe('Add function', () => {
     it('should return 5 when adding 2 and 3', () => {
         expect(add(2, 3)).to.equal(5);
     });
 });
+
 
